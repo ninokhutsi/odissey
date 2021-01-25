f47d66d65c3ab0d71f9325b1a6dec99d145986e7
ninokhutsi <ninokhutsiberidze16b@geolab.edu.ge>
2021-01-25 23:41:59 +0400

    mob resolution

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,15 @@
 var gulp = require('gulp');
-// Requires the gulp-sass plugin
 var sass = require('gulp-sass');
 
 
 gulp.task('sass', function(){
     return gulp.src('source-files')
-      .pipe(sass()) // Using gulp-sass
+      .pipe(sass()) 
       .pipe(gulp.dest('destination'))
   });
 
   gulp.task('sass', function(){
     return gulp.src('scss/styles.scss')
-      .pipe(sass()) // Converts Sass to CSS with gulp-sass
+      .pipe(sass()) 
       .pipe(gulp.dest('app/css'))
   });
